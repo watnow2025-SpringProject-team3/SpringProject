a97bd19f0efafb9f3b814c75ceeb13bc3ff93471
umiyuri777 <yuseiai15@gmail.com>
2025-06-27 14:21:49 +0900

    feat: フォントをfigma通りのNoto Serifに変更

--- loellingite/src/app/room/page.tsx
+++ loellingite/src/app/room/page.tsx
@@ -10,7 +10,7 @@ const rooms = Array.from({ length: 8 }, (_, i) => ({
 
 export default function RoomPage() {
     return (
-        <div className="bg-[#F7F5F2] min-h-screen font-sans">
+        <div className="bg-[#F7F5F2] min-h-screen font-Noto Serif">
             <header className="bg-[#7B5858] shadow-md sticky top-0 z-10 h-16 flex items-center justify-end px-4 sm:px-6" / >
             <RoomGrid rooms={rooms} />
         </div>
